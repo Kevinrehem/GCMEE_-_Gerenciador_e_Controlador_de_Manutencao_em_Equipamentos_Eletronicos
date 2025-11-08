0ce7c886f0e8cdac3af2ba063129d0ab3da09baf
Kevinrehem <kevin.rehem@gmail.com>
2025-11-07 22:07:31 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# OnboardingLandpage
---
+++ backend/src/main/java/com/jacare/onboardingsites/OnboardingsitesApplication.java
@@ -0,0 +1,13 @@
+package com.jacare.onboardingsites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingsitesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnboardingsitesApplication.class, args);
+	}
+
+}
